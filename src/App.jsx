1d377e9a22756d310eac498069cccf22d56b122d
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-08-26 17:59:46 -0300

    filtering by search input

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import CoinCard from "./components/CoinCard";
 import LimitSelector from "./components/LimitSelector";
+import FilterInput from "./components/FilterInput";
 const API_URL = import.meta.env.VITE_API_URL
 
 function App() {
@@ -30,21 +31,31 @@ function App() {
 
  
 
-  
+  const filteredCoins = coins.filter( (coin => {
+    return (
+      coin.name.toLowerCase().includes(filter.toLowerCase())
+    ||
+    coin.symbol.toLowerCase().includes(filter.toLowerCase())
+    )
+  }))
 
   return (
     <div>
       <h1>Here are the Coins:</h1>
       {loading && <p>Loading...</p>}
       {error && <div className="error">{error}</div>}
-
-      <LimitSelector limit={limit} onLimitChange={setLimit}/>
+      <div className="top-controls">
+        <FilterInput filter={filter} onFilterChange={setFilter}/>
+        <LimitSelector limit={limit} onLimitChange={setLimit}/>
+        
+      </div>
       
+
       {!loading && !error && (
         <main className="grid">
-          {coins.map((coin) => (
+          {filteredCoins.length > 0 ? filteredCoins.map((coin) => (
             <CoinCard coin={coin} key={coin.id}/>
-          ))}
+          )) : <p>No coins match your search!</p>}
         </main>
       )}
     </div>
---
+++ src/components/FilterInput.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FilterInput = ( {filter, onFilterChange } ) => {
+  return (
+    <div className='filter'>
+        <input 
+            type='text'
+            value={filter}
+            placeholder='Filter coins'
+            onChange={(e) => onFilterChange(e.target.value)}
+        />
+        
+
+    </div>
+  )
+}
+
+export default FilterInput
