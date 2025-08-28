34694f74accb8c6d62cd80cc0818f3f8c9961b2d
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-08-28 15:14:45 -0300

    tailwind install

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 import CoinCard from "./components/CoinCard";
 import LimitSelector from "./components/LimitSelector";
 import FilterInput from "./components/FilterInput";
+import SortSelector from "./components/SortSelector";
 const API_URL = import.meta.env.VITE_API_URL
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   const [error, setError] = useState(null);
   const [limit, setLimit] = useState(5);
   const [filter, setFilter] = useState('');
+  const [sortBy, setSortBy] = useState('market_cap_desc');
 
   useEffect ( ()=> {
     const fetchCoins = async () => {
@@ -17,7 +19,7 @@ function App() {
         const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`);
         if (!res.ok ) throw new Error('failed!!!');
         const data = await res.json();
-        console.log(data)
+        
         setCoins(data);
       } catch (err){
         setError(err.message);
@@ -37,16 +39,32 @@ function App() {
     ||
     coin.symbol.toLowerCase().includes(filter.toLowerCase())
     )
-  }))
+  })).slice().sort( (a, b) => {
+    switch(sortBy) {
+      case 'market_cap_desc':
+        return b.market_cap - a.market_cap;
+      case 'market_cap_asc':
+        return a.market_cap - b.market_cap;
+      case 'price_desc':
+        return b.current_price - a.current_price;
+      case 'price_asc':
+        return a.current_price - b.current_price;
+      case 'change_desc':
+        return b.price_change_percentage_24h - a.price_change_percentage_24h;
+      case 'change_asc':
+        return a.price_change_percentage_24h - b.price_change_percentage_24h;
+    }
+  })
 
   return (
     <div>
-      <h1>Here are the Coins:</h1>
+      <h1 className="text-center font-semibold">Crypto Coins Portal</h1>
       {loading && <p>Loading...</p>}
       {error && <div className="error">{error}</div>}
       <div className="top-controls">
         <FilterInput filter={filter} onFilterChange={setFilter}/>
         <LimitSelector limit={limit} onLimitChange={setLimit}/>
+        <SortSelector sortBy={sortBy} onSortChange={setSortBy}/>
         
       </div>
       
