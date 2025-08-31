9fa1ba4417075735fef7ad3c3172403391d33641
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-08-31 15:53:11 -0300

    tailwind fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import { Link} from 'react-router'
 
 const Navbar = () => {
   return (
-    <div className="flex justify-end gap-6 p-4shadow-md rounded-b-lg">
+    <div className="flex justify-end gap-6 p-4">
       <Link 
         to="/" 
-        className="text-gray-700 font-semibold hover:text-blue-500 transition-colors"
+        className="!text-gray-700 font-semibold hover:!text-pink-500 transition-colors"
       >
         Home
       </Link>
       <Link 
         to="/about" 
-        className="text-gray-700 font-semibold hover:text-blue-500 transition-colors"
+        className="!text-gray-700 font-semibold hover:!text-pink-500 transition-colors"
       >
         About
       </Link>
